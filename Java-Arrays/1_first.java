dcb2e69475e6c78843556986639433389d478ad8
Gopal Kumar <102371178+Gopal-Verma@users.noreply.github.com>
2023-04-23 00:41:51 +0530

    Create 1_first.java

---
+++ Java-Arrays/1_first.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        
+        int[] arr = new int[size];
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = sc.nextInt();
+            System.out.println(arr[i]);
+        }
+    }
+}
